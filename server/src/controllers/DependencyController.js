71cfd1b3bc19a4eec23ea7706b51e54076fb08ba
MJ van Staden <mj@agiledevsolutions.com>
2023-05-26 12:54:49 +0200

    Added Dependency Controller

---
+++ server/src/controllers/DependencyController.js
@@ -0,0 +1,64 @@
+const {Dependency} = require('../models')
+
+module.exports = {
+  async index (req, res) {
+    try {
+      let dependencies = null
+      const search = req.query.search
+      if (search) {
+        dependencies = await Dependency.findAll({
+          where: {
+            $or: [
+              'name', 
+            ].map(key => ({
+              [key]: {
+                $like: `%${search}%`
+              }
+            }))
+          }
+        })
+      } else {
+        dependencies = await Dependency.findAll()
+      }
+      res.send(dependencies)
+    } catch (err) {
+      res.status(500).send({
+        error: 'an error has occured trying to fetch the dependencies'
+      })
+    }
+  },
+  async show (req, res) {
+    try {
+      const dependency = await Dependency.findByPk(req.params.dependencyId)
+      res.send(dependency)
+    } catch (err) {
+      res.status(500).send({
+        error: 'an error has occured trying to show the dependency'
+      })
+    }
+  },
+  async post (req, res) {
+    try {
+      const dependency = await Dependency.create(req.body)
+      res.send(dependency)
+    } catch (err) {
+      res.status(500).send({
+        error: 'an error has occured trying to create the dependency'
+      })
+    }
+  },
+  async put (req, res) {
+    try {
+      await dependency.update(req.body, {
+        where: {
+          id: req.params.dependencyId
+        }
+      })
+      res.send(req.body)
+    } catch (err) {
+      res.status(500).send({
+        error: 'an error has occured trying to update the dependency'
+      })
+    }
+  }
+}
